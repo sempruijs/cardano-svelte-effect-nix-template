287320682609f9cca434a25ede2f99d8d07f12fa
sem pruijs <sem@pruijs.net>
2025-05-05 15:16:15 +0200

    remove all css

--- site/src/lib/components/UtxoDisplay.svelte
+++ site/src/lib/components/UtxoDisplay.svelte
@@ -4,14 +4,14 @@
   export let utxo: Utxo;
 </script>
 
-<div class="mesh-bg-gray-800 mesh-rounded-lg mesh-p-4 mesh-my-2 mesh-text-left">
+<div class="">
   <p><strong>Tx Hash:</strong> {utxo.input.txHash}</p>
   <p><strong>Output Index:</strong> {utxo.input.outputIndex}</p>
   <p><strong>Address:</strong> {utxo.output.address}</p>
 
-  <div class="mesh-mt-2">
+  <div class="">
     <strong>Amount:</strong>
-    <ul class="mesh-text-sm mesh-text-gray-300">
+    <ul class="">
       {#each utxo.output.amount as asset}
         <li>{asset.unit}: {asset.quantity}</li>
       {/each}
--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -41,18 +41,18 @@
     });
 </script>
 
-<div class="mesh-bg-gray-900 mesh-w-full mesh-text-white mesh-text-center">
+<div class="">
     <main
-        class={`mesh-flex mesh-min-h-screen mesh-flex-col mesh-items-center mesh-justify-center mesh-p-24`}
+        class={``}
     >
-        <h1 class="mesh-text-6xl mesh-font-thin mesh-mb-20">
+        <h1 class="">
             <a href="https://meshjs.dev/" class="mesh-text-sky-600">
                 Mesh
             </a>{" "}
             SvelteKit
         </h1>
 
-        <div class="mesh-mb-20">
+        <div class="">
             <CardanoWallet isDark={true} />
             <button onclick={send_ada}>send</button>
             <LearnMore />
@@ -64,16 +64,16 @@
         </div>
 
         <div
-            class="mesh-grid mesh-grid-cols-1 md:mesh-grid-cols-2 lg:mesh-grid-cols-3 mesh-content-center mesh-justify-around"
+            class=""
         >
             <a
                 href="https://meshjs.dev/apis"
-                class="mesh-bg-gray-800 mesh-rounded-xl mesh-border mesh-border-white hover:mesh-scale-105 mesh-transition mesh-max-w-96 mesh-p-5 mesh-m-5"
+                class=""
             >
-                <h2 class="mesh-text-2xl mesh-font-bold mesh-mb-2">
+                <h2 class="">
                     Documentation
                 </h2>
-                <p class="mesh-text-gray-400">
+                <p class="">
                     Our documentation provide live demos and code samples; great
                     educational tool for learning how Cardano works.
                 </p>
@@ -81,10 +81,10 @@
 
             <a
                 href="https://meshjs.dev/guides"
-                class="mesh-bg-gray-800 mesh-rounded-xl mesh-border mesh-border-white hover:mesh-scale-105 mesh-transition mesh-max-w-96 mesh-p-5 mesh-m-5"
+                class=""
             >
-                <h2 class="mesh-text-2xl mesh-font-bold mesh-mb-2">Guides</h2>
-                <p class="mesh-text-gray-400">
+                <h2 class="">Guides</h2>
+                <p class="">
                     Whether you are launching a new NFT project or ecommerce
                     store, these guides will help you get started.
                 </p>
@@ -92,12 +92,12 @@
 
             <a
                 href="https://meshjs.dev/svelte"
-                class="mesh-bg-gray-800 mesh-rounded-xl mesh-border mesh-border-white hover:mesh-scale-105 mesh-transition mesh-max-w-96 mesh-p-5 mesh-m-5 md:mesh-mx-auto lg:mesh-mx-5 md:mesh-col-span-2 lg:mesh-col-span-1"
+                class=""
             >
-                <h2 class="mesh-text-2xl mesh-font-bold mesh-mb-2">
+                <h2 class="">
                     Svelte components
                 </h2>
-                <p class="mesh-text-gray-400">
+                <p class="">
                     Useful Svelte UI components, seamlessly integrate them into
                     your app, and bring the user interface to life.
                 </p>
@@ -105,6 +105,6 @@
         </div>
     </main>
     <footer
-        class="mesh-p-8 mesh-border-t mesh-border-gray-300 mesh-flex mesh-justify-center"
+        class=""
     ></footer>
 </div>
